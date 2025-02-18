27f15c622983c70110308c3b0251ced6a571d3ff
Eric Lang <94770511+88potatoes@users.noreply.github.com>
2025-02-18 16:28:10 +1100

    (feat) home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,26 @@ export default function Home() {
   return (
     <>
       <TopBar />
-      <h1>hi</h1>
+      <div className="container mx-auto mt-6 flex flex-col gap-4">
+        <a
+          href="/test"
+          className="bg-gray-800 text-white py-3 px-4 rounded-lg text-center"
+        >
+          Test
+        </a>
+        <a
+          href="/results"
+          className="bg-gray-800 text-white py-3 px-4 rounded-lg text-center"
+        >
+          Results
+        </a>
+        <a
+          href="/selection"
+          className="bg-gray-800 text-white py-3 px-4 rounded-lg text-center"
+        >
+          Selection
+        </a>
+      </div>
     </>
   );
 }
